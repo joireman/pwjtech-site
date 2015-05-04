332c05d8e788845a4ccef6396605284654211e4b
Paul W. Joireman <paul.joireman@vgbio.com>
2015-05-03 20:40:22 -0500

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Paul W. Joireman'
+SITENAME = u'Technically Me'
+SITEURL = 'pwjtech.net'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Site by Pelican', 'http://getpelican.com/'),
+         ('Powered by Python', 'http://python.org/'),
+         ('physIQ, Inc.', 'http://www.physiq.com'),)
+
+# Social widget
+SOCIAL = (('LinkedIn Profile', 'https://www.linkedin.com/hp/?dnr=JmR3tlzLTGfceYkC3gs3A5z5TdEkHp-Dd8A'),
+        ('@PaulJoireman', 'https://twitter.com/PaulJoireman'),)
+
+STATIC_PATHS = ['images', 'pdfs']
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
